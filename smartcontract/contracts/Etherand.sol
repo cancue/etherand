73c58595d62ed255e8049a0a1f4ee295a7a931d8
cancue <kanqmh@gmail.com>
2019-03-05 16:26:11 +0900

    Add callFor

--- smartcontract/contracts/Etherand.sol
+++ smartcontract/contracts/Etherand.sol
@@ -15,6 +15,15 @@ contract Committee {
     emit SetCommittee(_committee);
   }
 
+  function callFor(address _to, uint256 _gas, bytes calldata _code)
+    external
+    payable
+    onlyCommittee
+    returns (bool _result)
+  {
+    (_result,) = _to.call.value(msg.value).gas(_gas)(_code);
+  }
+
   /* MODIFIER */
   modifier onlyCommittee {
     require(msg.sender == committee);
